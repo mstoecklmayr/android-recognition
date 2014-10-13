9ae3440d1759674a5a2921904e3fe9b652e21bac
mike <mstocklmayr@uclan.ac.uk>
2014-10-13 10:23:15 +0100

    Project skeleton

--- AndroidTextRecognition/src/com/uclan/mstocklmayr/MyActivity.java
+++ AndroidTextRecognition/src/com/uclan/mstocklmayr/MyActivity.java
@@ -1,7 +1,11 @@
 package com.uclan.mstocklmayr;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.googlecode.tesseract.android.TessBaseAPI;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +15,19 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        //test
+        TessBaseAPI api = null;
+
+        Button btn_test = (Button) findViewById(R.id.btn_test);
+        btn_test.setOnClickListener(btn_click);
     }
+
+    View.OnClickListener btn_click = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MyActivity.this, CaptureActivity.class);
+            startActivity(intent);
+        }
+    };
 }
